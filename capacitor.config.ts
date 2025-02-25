7d3a265b33991116ef4fab6454ce63ea81d07855
juanmariiaa <arizaserranojuanmaria@gmail.com>
2025-02-25 17:44:09 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'warmace-manager',
+  webDir: 'www'
+};
+
+export default config;
