833c6eb1ced8a78b2b2ef81ed3eca2bfac50d56f
juanmariiaa <arizaserranojuanmaria@gmail.com>
2025-03-06 13:28:52 +0100

    Terminado gestor de inventario

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'warmace-manager',
-  webDir: 'www'
+  appName: 'inventario-manager',
+  webDir: 'www',
 };
 
 export default config;
